9e6203ed5796228a306fcbffba1d69f514ba49f0
Donna Torres <todonnatorres@icloud.com>
2020-07-13 23:46:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,17 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get '/' do
+      
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'  
+
+    end
+
     # code other routes/actions here
 
   end
